954f788c674b57b17b56902320cfe9a231325488
Palash <palash.kanti.kundu.90@gmail.com>
2019-01-20 10:37:24 +0530

    Added BootStrap and some path for testing

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <router-link to="/" exact>world</router-link>
+    <router-link to="/app">App</router-link>
     <router-view/>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Avatar from '@/components/Avatar'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    }, {
+      path:"/app",
+      name: "Avatar",
+      component: Avatar
     }
   ]
 })
