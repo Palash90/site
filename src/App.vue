84e5991e5aa7c13ce151e912c0d4b25227999fbe
Palash <palash.kanti.kundu.90@gmail.com>
2019-01-20 13:37:42 +0530

    icon finalized

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <router-link to="/" exact>world</router-link>
     <router-link to="/app">App</router-link>
     <router-view/>
@@ -8,17 +8,6 @@
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
