1b0316839a0d0e022c3ffd91728f955700053f69
Palash <palash.kanti.kundu.90@gmail.com>
2019-01-22 09:55:34 +0530

    Added React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="center-div">
+        <img alt="Palash Kanti Kundu" src="/assets/nameImage.png"></img>
+      </div>
+    );
+  }
+}
+
+export default App;
