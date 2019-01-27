46fad3f101f0881e6c431d5bb444a1a86d3e3ff0
Palash <palash.kanti.kundu.90@gmail.com>
2019-01-27 12:43:46 +0530

    Added Good Reads

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import GoodReads from './Components/GoodReads/GoodReads';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <p>Palash Kanti Kundu</p>
+        <GoodReads />
       </div>
     );
   }
---
+++ src/Components/GoodReads/GoodReads.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import './GoodReads.css';
+
+export default class GoodReads extends Component {
+    render() {
+        return (
+            <div>
+                <div id="gr_custom_widget_">
+                    <div className="gr_custom_container_">
+                        <h2 className="gr_custom_header_">
+                            <a style={{ 'textDecoration': 'none' }} rel="nofollow" href="https://www.goodreads.com/review/list/47890790-palash-kundu?shelf=read&amp;utm_medium=api&amp;utm_source=custom_widget">Palash&#39;s books</a>
+                        </h2>
+                        <div className="gr_custom_each_container_">
+                            <div className="gr_custom_book_container_">
+                                <a title="My Life: An Illustrated Biography" rel="nofollow" href="https://www.goodreads.com/review/show/2638926195?utm_medium=api&amp;utm_source=custom_widget"><img alt="My Life: An Illustrated Biography" border="0" src="https://images.gr-assets.com/books/1447218083s/27797706.jpg" /></a>
+                            </div>
+                            <div className="gr_custom_rating_">
+                                <span className=" staticStars" title="it was amazing"><img alt="it was amazing" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /></span>
+                            </div>
+                            <div className="gr_custom_title_">
+                                <a rel="nofollow" href="https://www.goodreads.com/review/show/2638926195?utm_medium=api&amp;utm_source=custom_widget">My Life: An Illustrated Biography</a>
+                            </div>
+                            <div className="gr_custom_author_">
+                                by <a rel="nofollow" href="https://www.goodreads.com/author/show/986042.A_P_J_Abdul_Kalam">A.P.J. Abdul Kalam</a>
+                            </div>
+                        </div>
+                        <div className="gr_custom_each_container_">
+                            <div className="gr_custom_book_container_">
+                                <a title="Ghanada Samagra - Vol.1" rel="nofollow" href="https://www.goodreads.com/review/show/2571990501?utm_medium=api&amp;utm_source=custom_widget"><img alt="Ghanada Samagra - Vol.1" border="0" src="https://images.gr-assets.com/books/1524329544s/39908291.jpg" /></a>
+                            </div>
+                            <div className="gr_custom_rating_">
+                                <span className=" staticStars" title="really liked it"><img alt="really liked it" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /></span>
+                            </div>
+                            <div className="gr_custom_title_">
+                                <a rel="nofollow" href="https://www.goodreads.com/review/show/2571990501?utm_medium=api&amp;utm_source=custom_widget">Ghanada Samagra - Vol.1</a>
+                            </div>
+                            <div className="gr_custom_author_">
+                                by <a rel="nofollow" href="https://www.goodreads.com/author/show/635282.Premendra_Mitra">Premendra Mitra</a>
+                            </div>
+                            <div className="gr_custom_review_">
+
+                            </div>
+                        </div>
+                        <div className="gr_custom_each_container_">
+                            <div className="gr_custom_book_container_">
+                                <a title="The Blue Umbrella" rel="nofollow" href="https://www.goodreads.com/review/show/2571975261?utm_medium=api&amp;utm_source=custom_widget"><img alt="The Blue Umbrella" border="0" src="https://images.gr-assets.com/books/1442688976s/26716989.jpg" /></a>
+                            </div>
+                            <div className="gr_custom_rating_">
+                                <span className=" staticStars" title="it was amazing"><img alt="it was amazing" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /></span>
+                            </div>
+                            <div className="gr_custom_title_">
+                                <a rel="nofollow" href="https://www.goodreads.com/review/show/2571975261?utm_medium=api&amp;utm_source=custom_widget">The Blue Umbrella</a>
+                            </div>
+                            <div className="gr_custom_author_">
+                                by <a rel="nofollow" href="https://www.goodreads.com/author/show/46603.Ruskin_Bond">Ruskin Bond</a>
+                            </div>
+                        </div>
+                        <div className="gr_custom_each_container_">
+                            <div className="gr_custom_book_container_">
+                                <a title="Subconscious Mind Power: How to Use the Hidden Power of Your Subconscious Mind" rel="nofollow" href="https://www.goodreads.com/review/show/2506728703?utm_medium=api&amp;utm_source=custom_widget"><img alt="Subconscious Mind Power: How to Use the Hidden Power of Your Subconscious Mind" border="0" src="https://images.gr-assets.com/books/1427618293s/19012581.jpg" /></a>
+                            </div>
+                            <div className="gr_custom_rating_">
+                                <span className=" staticStars" title="liked it"><img alt="liked it" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /></span>
+                            </div>
+                            <div className="gr_custom_title_">
+                                <a rel="nofollow" href="https://www.goodreads.com/review/show/2506728703?utm_medium=api&amp;utm_source=custom_widget">Subconscious Mind Power: How to Use the Hidden Power of Your Subconscious Mind</a>
+                            </div>
+                            <div className="gr_custom_author_">
+                                by <a rel="nofollow" href="https://www.goodreads.com/author/show/8061906.James_Thompson">James             Thompson</a>
+                            </div>
+                        </div>
+                        <div className="gr_custom_each_container_">
+                            <div className="gr_custom_book_container_">
+                                <a title="Aesop's Fables" rel="nofollow" href="https://www.goodreads.com/review/show/2499610698?utm_medium=api&amp;utm_source=custom_widget"><img alt="Aesop's Fables" border="0" src="https://images.gr-assets.com/books/1328326471s/11052237.jpg" /></a>
+                            </div>
+                            <div className="gr_custom_rating_">
+                                <span className=" staticStars" title="it was amazing"><img alt="it was amazing" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_active.png" /></span>
+                            </div>
+                            <div className="gr_custom_title_">
+                                <a rel="nofollow" href="https://www.goodreads.com/review/show/2499610698?utm_medium=api&amp;utm_source=custom_widget">Aesop's Fables</a>
+                            </div>
+                            <div className="gr_custom_author_">
+                                by <a rel="nofollow" href="https://www.goodreads.com/author/show/12452.Aesop">Aesop</a>
+                            </div>
+                        </div>
+                        <br style={{ "clear": "both" }} />
+                        <center>
+                            <a rel="nofollow" href="https://www.goodreads.com/"><img alt="goodreads.com" style={{ "border": 0 }} src="https://www.goodreads.com/images/widget/widget_logo.gif" /></a>
+                        </center>
+                    </div>
+                </div>
+                <script src="https://www.goodreads.com/review/custom_widget/47890790.Palash's%20bookshelf:%20read?cover_position=&cover_size=&num_books=5&order=&shelf=&sort=&widget_bg_transparent=" type="text/javascript" charSet="utf-8"></script>
+            </div>
+        );
+    }
+}
