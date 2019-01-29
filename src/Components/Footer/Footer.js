abc61380beb4bd338f8e0f13c1f9e27c4fd004f6
Palash <palash.kanti.kundu.90@gmail.com>
2019-01-29 10:31:07 +0530

    Fix violation and add a tinge of red to footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,25 +5,25 @@ export default function Footer(props) {
         <footer className="footer">
             <div className="d-flex justify-content-center">
                 <div className="p-2">
-                    <img src="./static/si-glyph-smartphone.svg" />
+                    <img src="./static/si-glyph-smartphone.svg" alt="Phone: " />
                 </div>
                 <div className="p-2">
                     <span className="text-danger">+919851800599</span>
                 </div>
                 <div className="p-2">
-                    <img src="./static/si-glyph-mail.svg" />
+                    <img src="./static/si-glyph-mail.svg" alt="Mail: " />
                 </div>
                 <div className="p-2">
                     <a href="mailto:me@palash90.in" className="text-danger">me@palash90.in</a>
                 </div>
                 <div className="p-2">
-                    <img src="./static/si-glyph-button-plus.svg" />
+                    <img src="./static/si-glyph-button-plus.svg" alt="LinkedIn: " />
                 </div>
                 <div className="p-2">
                     <a className="text-danger" href="http://www.linkedin.com/in/palash90">Add me on LinkedIn</a>
                 </div>
                 <div className="p-2">
-                    <img src="./static/copyright.svg" />
+                    <img src="./static/copyright.svg" alt="Copyright: " />
                 </div>
                 <div className="p-2">
                     <span className="text-danger">2019 Palash Kanti Kundu</span>
