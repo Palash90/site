943833c1b03d8da363abe58901701766f3b46813
Palash <palash.kanti.kundu.90@gmail.com>
2019-02-11 19:15:28 +0530

    Resize plot on window resize

--- src/Components/CustomPlot/CustomPlot.js
+++ src/Components/CustomPlot/CustomPlot.js
@@ -5,14 +5,30 @@ import Plot from 'react-plotly.js';
 export default class CustomPlot extends React.Component {
     constructor(props) {
         super(props);
+        this.updateDimensions = this.updateDimensions.bind(this);
+        this.updateState = this.updateState.bind(this);
         this.state = {
             layout: this.props.data.layout
         };
+        this.updateState();
+    }
+
+    componentDidMount() {
+        this.updateDimensions();
+        window.addEventListener("resize", this.updateDimensions.bind(this));
+    }
+
+    updateDimensions() {
+        this.updateState();
+    }
+    updateState() {
         var state = this.state.layout;
         state['paper_bgcolor'] = 'rgba(0,0,0,0)';
         state['plot_bgcolor'] = 'rgba(0,0,0,0)';
-
+        state['width'] = (window.innerWidth * 98 / 100);
+        this.setState({layout:state})
     }
+
     render() {
         return (
             <Plot
