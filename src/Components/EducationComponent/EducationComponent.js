ac02d9ae4ba89130d3ed96c90e812858ea42431c
Palash <palash.kanti.kundu.90@gmail.com>
2019-05-23 06:24:11 +0530

    Added portfolio template

--- src/Components/EducationComponent/EducationComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Infolet from '../Infolet/Infolet';
-import educationData from '../../Data/EducationData';
-
-export default function EducationComponent(props) {
-    return (
-        <Infolet tableData={educationData.tableData} plotData={educationData.plotData} changeOn={educationData.changeMode}/>
-    );
-}
