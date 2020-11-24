e64dce463e88884e57542c85614eeb20e2711a4e
Palash <palash.kanti.kundu.90@gmail.com>
2020-11-24 20:31:05 +0530

    Added calculations

--- public/card_assets/data.js
+++ public/card_assets/data.js
@@ -380,6 +380,297 @@ var bengaliAlphabets = [
   }
 ];
 
+var hindiAlphabets = [
+  {
+    content: "&#x0905;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0906;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0907;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0908;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0909;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x090A;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x090B;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x090F;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0910;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0913;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0914;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "अं",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "अः",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0915;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0916;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0917;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0918;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0919;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x091A;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x091B;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x091C;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x091D;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x091E;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x091F;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0920;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0921;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0922;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0923;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0924;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0925;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0926;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0927;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0928;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x092A;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x092B;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x092C;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x092D;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x092E;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x092F;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0930;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0932;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0935;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0936;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0937;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0938;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x0939;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x095C;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  },{
+    content: "&#x095D;",
+    type: "alphabet",
+    desc: {
+      language: "Hindi"
+    }
+  }]
+
 var plus = [
   {
     content: "2+2",
@@ -397,7 +688,67 @@ var plus = [
   {
     content: "7+9",
     type: "calculation"
+  },
+  {
+    content: "79+9",
+    type: "calculation"
+  },
+  {
+    content: "19+9",
+    type: "calculation"
+  },{
+    content: "59+8",
+    type: "calculation"
+  },{
+    content: "47+6",
+    type: "calculation"
+  },{
+    content: "48+19",
+    type: "calculation"
+  },,{
+    content: "94+13",
+    type: "calculation"
   }
 ];
 
-var contents = bengaliAlphabets.concat(plus);
+var minus = [
+  {
+    content: "12-2",
+    type: "calculation",
+    answer: "4"
+  },
+  {
+    content: "17-2",
+    type: "calculation"
+  },
+  {
+    content: "76-12",
+    type: "calculation"
+  },
+  {
+    content: "17-9",
+    type: "calculation"
+  },
+  {
+    content: "79-9",
+    type: "calculation"
+  },
+  {
+    content: "19-12",
+    type: "calculation"
+  },{
+    content: "59-18",
+    type: "calculation"
+  },{
+    content: "47-6",
+    type: "calculation"
+  },{
+    content: "48-9",
+    type: "calculation"
+  },,{
+    content: "94-13",
+    type: "calculation"
+  }
+];
+
+var contents = plus.concat(hindiAlphabets).concat(bengaliAlphabets).concat(minus);
