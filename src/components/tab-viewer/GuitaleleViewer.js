7185292fa724d38a1849e03615025a1ef7d648d6
palash90 <palash.kanti.kundu.90@gmail.com>
2026-06-07 15:48:40 +0530

    Remove unnecessary imports

--- src/components/tab-viewer/GuitaleleViewer.js
+++ src/components/tab-viewer/GuitaleleViewer.js
@@ -1,5 +1,4 @@
 import React, { useState, useMemo, useRef, useEffect } from "react";
-import { playHumanizedGuitaleleNote, TUNING } from "./audio";
 import {
     DARK_THEME,
     calculateSchedulerBoundaries,
